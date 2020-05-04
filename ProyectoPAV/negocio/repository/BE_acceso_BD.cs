fe3a7187289f952af4fb588c5d5d1a03b10c22cd
Dario Marquez <darioemanuelmarquez@gmail.com>
2020-05-04 11:47:50 -0300

    Upload project

---
+++ ProyectoPAV/negocio/repository/BE_acceso_BD.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.OleDb;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoPAV.negocio.repository
+{
+    class BE_acceso_BD
+    {
+        OleDbConnection conexion = new OleDbConnection();
+        OleDbCommand cmd = new OleDbCommand();
+        string cadenaConexion = "Provider=SQLNCLI10;Data Source=DESKTOP-OKDL643\\SQLEXPRESS;Integrated Security=SSPI;Initial Catalog=ProyectoPAV1";
+
+        private void conectar()
+        {
+            conexion.ConnectionString = cadenaConexion;
+            conexion.Open();
+            cmd.Connection = conexion;
+            cmd.CommandType = CommandType.Text;
+        }
+        private void desconectar()
+        {
+
+            conexion.Close();
+
+        }
+
+        public DataTable ejecutarConsulta(string consultaSQL)
+        {
+            conectar();
+            DataTable tabla = new DataTable();
+            cmd.CommandText = consultaSQL;
+            tabla.Load(cmd.ExecuteReader());
+            desconectar();
+            return tabla;
+        }
+    }
+}
---
+++ ProyectoPAV/negocio/repository/NavioDao.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoPAV.negocio.repository
+{
+    class NavioDao
+    {
+        BE_acceso_BD accesoBD = new BE_acceso_BD();
+
+        private bool create(Navio navio)
+        {
+            string strSQL = "";
+        }
+    }
+}
