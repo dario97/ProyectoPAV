2135b6153f949b367c39e1f8b4650b2de0cb7577
Alvaro <vegaa8238@gmail.com>
2020-05-06 12:09:53 -0300

    tab

--- ProyectoPAV/gui/frm_abmc_tipoNavio.Designer.cs
+++ ProyectoPAV/gui/frm_abmc_tipoNavio.Designer.cs
@@ -30,16 +30,16 @@
         {
             this.botonConsultar = new System.Windows.Forms.Button();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.chk_todos = new System.Windows.Forms.CheckBox();
+            this.botonSalir = new System.Windows.Forms.Button();
+            this.botonEliminar = new System.Windows.Forms.Button();
+            this.botonModificar = new System.Windows.Forms.Button();
             this.registrarTipo = new System.Windows.Forms.Button();
             this.dgv1 = new System.Windows.Forms.DataGridView();
-            this.label_tipoNavio = new System.Windows.Forms.Label();
-            this.txt_tipoNavio = new System.Windows.Forms.TextBox();
-            this.botonModificar = new System.Windows.Forms.Button();
-            this.botonEliminar = new System.Windows.Forms.Button();
-            this.botonSalir = new System.Windows.Forms.Button();
-            this.chk_todos = new System.Windows.Forms.CheckBox();
             this.ID = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Descripcion = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.label_tipoNavio = new System.Windows.Forms.Label();
+            this.txt_tipoNavio = new System.Windows.Forms.TextBox();
             this.groupBox1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dgv1)).BeginInit();
             this.SuspendLayout();
@@ -49,7 +49,7 @@
             this.botonConsultar.Location = new System.Drawing.Point(289, 34);
             this.botonConsultar.Name = "botonConsultar";
             this.botonConsultar.Size = new System.Drawing.Size(75, 23);
-            this.botonConsultar.TabIndex = 0;
+            this.botonConsultar.TabIndex = 2;
             this.botonConsultar.Text = "Consultar";
             this.botonConsultar.UseVisualStyleBackColor = true;
             this.botonConsultar.Click += new System.EventHandler(this.botonConsultar_Click);
@@ -68,9 +68,49 @@
             this.groupBox1.Location = new System.Drawing.Point(12, 12);
             this.groupBox1.Name = "groupBox1";
             this.groupBox1.Size = new System.Drawing.Size(388, 307);
-            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabIndex = 0;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Tipos de Navios";
+            // 
+            // chk_todos
+            // 
+            this.chk_todos.AutoSize = true;
+            this.chk_todos.Location = new System.Drawing.Point(110, 64);
+            this.chk_todos.Name = "chk_todos";
+            this.chk_todos.Size = new System.Drawing.Size(56, 17);
+            this.chk_todos.TabIndex = 3;
+            this.chk_todos.Text = "Todos";
+            this.chk_todos.UseVisualStyleBackColor = true;
+            // 
+            // botonSalir
+            // 
+            this.botonSalir.Location = new System.Drawing.Point(298, 278);
+            this.botonSalir.Name = "botonSalir";
+            this.botonSalir.Size = new System.Drawing.Size(75, 23);
+            this.botonSalir.TabIndex = 7;
+            this.botonSalir.Text = "Salir";
+            this.botonSalir.UseVisualStyleBackColor = true;
+            this.botonSalir.Click += new System.EventHandler(this.botonSalir_Click);
+            // 
+            // botonEliminar
+            // 
+            this.botonEliminar.Location = new System.Drawing.Point(167, 244);
+            this.botonEliminar.Name = "botonEliminar";
+            this.botonEliminar.Size = new System.Drawing.Size(75, 23);
+            this.botonEliminar.TabIndex = 6;
+            this.botonEliminar.Text = "Eliminar";
+            this.botonEliminar.UseVisualStyleBackColor = true;
+            this.botonEliminar.Click += new System.EventHandler(this.botonEliminar_Click);
+            // 
+            // botonModificar
+            // 
+            this.botonModificar.Location = new System.Drawing.Point(87, 244);
+            this.botonModificar.Name = "botonModificar";
+            this.botonModificar.Size = new System.Drawing.Size(75, 23);
+            this.botonModificar.TabIndex = 5;
+            this.botonModificar.Text = "Modificar";
+            this.botonModificar.UseVisualStyleBackColor = true;
+            this.botonModificar.Click += new System.EventHandler(this.botonModificar_Click);
             // 
             // registrarTipo
             // 
@@ -94,62 +134,6 @@
             this.dgv1.TabIndex = 3;
             this.dgv1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
             // 
-            // label_tipoNavio
-            // 
-            this.label_tipoNavio.AutoSize = true;
-            this.label_tipoNavio.Location = new System.Drawing.Point(30, 40);
-            this.label_tipoNavio.Name = "label_tipoNavio";
-            this.label_tipoNavio.Size = new System.Drawing.Size(74, 13);
-            this.label_tipoNavio.TabIndex = 2;
-            this.label_tipoNavio.Text = "Tipo de navío";
-            // 
-            // txt_tipoNavio
-            // 
-            this.txt_tipoNavio.Location = new System.Drawing.Point(110, 37);
-            this.txt_tipoNavio.Name = "txt_tipoNavio";
-            this.txt_tipoNavio.Size = new System.Drawing.Size(173, 20);
-            this.txt_tipoNavio.TabIndex = 1;
-            // 
-            // botonModificar
-            // 
-            this.botonModificar.Location = new System.Drawing.Point(87, 244);
-            this.botonModificar.Name = "botonModificar";
-            this.botonModificar.Size = new System.Drawing.Size(75, 23);
-            this.botonModificar.TabIndex = 5;
-            this.botonModificar.Text = "Modificar";
-            this.botonModificar.UseVisualStyleBackColor = true;
-            this.botonModificar.Click += new System.EventHandler(this.botonModificar_Click);
-            // 
-            // botonEliminar
-            // 
-            this.botonEliminar.Location = new System.Drawing.Point(167, 244);
-            this.botonEliminar.Name = "botonEliminar";
-            this.botonEliminar.Size = new System.Drawing.Size(75, 23);
-            this.botonEliminar.TabIndex = 6;
-            this.botonEliminar.Text = "Eliminar";
-            this.botonEliminar.UseVisualStyleBackColor = true;
-            this.botonEliminar.Click += new System.EventHandler(this.botonEliminar_Click);
-            // 
-            // botonSalir
-            // 
-            this.botonSalir.Location = new System.Drawing.Point(298, 278);
-            this.botonSalir.Name = "botonSalir";
-            this.botonSalir.Size = new System.Drawing.Size(75, 23);
-            this.botonSalir.TabIndex = 7;
-            this.botonSalir.Text = "Salir";
-            this.botonSalir.UseVisualStyleBackColor = true;
-            this.botonSalir.Click += new System.EventHandler(this.botonSalir_Click);
-            // 
-            // chk_todos
-            // 
-            this.chk_todos.AutoSize = true;
-            this.chk_todos.Location = new System.Drawing.Point(110, 64);
-            this.chk_todos.Name = "chk_todos";
-            this.chk_todos.Size = new System.Drawing.Size(56, 17);
-            this.chk_todos.TabIndex = 8;
-            this.chk_todos.Text = "Todos";
-            this.chk_todos.UseVisualStyleBackColor = true;
-            // 
             // ID
             // 
             this.ID.HeaderText = "ID";
@@ -160,6 +144,22 @@
             this.Descripcion.HeaderText = "Descripción";
             this.Descripcion.Name = "Descripcion";
             this.Descripcion.Width = 400;
+            // 
+            // label_tipoNavio
+            // 
+            this.label_tipoNavio.AutoSize = true;
+            this.label_tipoNavio.Location = new System.Drawing.Point(30, 40);
+            this.label_tipoNavio.Name = "label_tipoNavio";
+            this.label_tipoNavio.Size = new System.Drawing.Size(74, 13);
+            this.label_tipoNavio.TabIndex = 0;
+            this.label_tipoNavio.Text = "Tipo de navío";
+            // 
+            // txt_tipoNavio
+            // 
+            this.txt_tipoNavio.Location = new System.Drawing.Point(110, 37);
+            this.txt_tipoNavio.Name = "txt_tipoNavio";
+            this.txt_tipoNavio.Size = new System.Drawing.Size(173, 20);
+            this.txt_tipoNavio.TabIndex = 1;
             // 
             // frm_abmc_tipoNavio
             // 
