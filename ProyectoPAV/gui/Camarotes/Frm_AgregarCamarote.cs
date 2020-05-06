f63e8f334fc66f0df166be269f10d928d73eb34a
Admin <Admin@DESKTOP-3MQ0FV8>
2020-05-06 18:45:49 -0300

    hmfx

--- ProyectoPAV/gui/Camarotes/Frm_AgregarCamarote.cs
+++ ProyectoPAV/gui/Camarotes/Frm_AgregarCamarote.cs
@@ -12,6 +12,8 @@ namespace ProyectoPAV.gui
 {
     public partial class Frm_AgregarCamarote : Form
     {
+        private static CamaroteService _camarote = new CamaroteService; 
+
         public Frm_AgregarCamarote()
         {
             InitializeComponent();
@@ -30,6 +32,36 @@ namespace ProyectoPAV.gui
         private void button2_Click(object sender, EventArgs e)
         {
             this.Close();
+        }
+
+        private void Frm_AgregarCamarote_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void cmd_aceptar_Click(object sender, EventArgs e)
+        {
+            if (_camarote.consultarCamarote(this.txt_codigo.Text, this.txt_cubierta.Text, this.txt_numcam.Text).Rows.Count == 0)
+            {
+                int codNavio = Convert.ToInt32(this.txt_codigo.Text);
+                int numCubierta = Convert.ToInt32(this.txt_cubierta.Text);
+                int numCamarote = Convert.ToInt32(this.txt_numcam.Text);
+                int tipo = Convert.ToInt32(this.txt_tipo.Text);
+                string ubicacion = this.txt_ubicacion.Text;
+                int cantCamas = Convert.ToInt32(this.txt_camas.Text);
+
+                Camarote camarote = new Camarote(codNavio, numCubierta, numCamarote, tipo, ubicacion, cantCamas);
+
+                _camarote.crearCamarote(camarote);
+
+                MessageBox.Show("El Camarote se guardó exitosamente", "Mensaje", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+
+                this.Close();
+            }
+            else
+            {
+                MessageBox.Show("El camarote ya existe, verifique los datos", "Mensaje", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
         }
     }
 }
