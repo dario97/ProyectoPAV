51b98d93e1a282d1ab18ac66e63319933b5b2daa
Javier Alejandro Piotti Trespi <75667@sistemas.frc.utn.edu.ar>
2020-05-06 18:38:20 -0300

    metodo agregar puertos

--- ProyectoPAV/gui/puertos/frm_alta_puertos.cs
+++ ProyectoPAV/gui/puertos/frm_alta_puertos.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ProyectoPAV.negocio.servicios;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,6 +13,7 @@ namespace ProyectoPAV.entidades
 {
     public partial class frm_alta_puertos : Form
     {
+        PuertoService puerto = new PuertoService();
         public frm_alta_puertos()
         {
             InitializeComponent();
@@ -21,5 +23,31 @@ namespace ProyectoPAV.entidades
         {
             this.Close();
         }
+
+        private void cmd_crear_Click(object sender, EventArgs e)
+        {
+            if(this.txt_nombre.Text=="")
+            {
+                MessageBox.Show("No se ingresó ningún dato", "Mensaje", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                txt_nombre.Focus();
+            }
+            else
+            {
+                if(puerto.consultarPorNombre(this.txt_nombre.Text).Rows.Count==0)
+                {
+                    string nombre = this.txt_nombre.Text;
+                    
+                    Puerto puerto1 = new Puerto(nombre);
+                    puerto.crearPuerto(puerto1);
+                    MessageBox.Show("Se creó el puerto " + nombre + " Exitosamente.", "Mensaje", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                    this.Close();
+                }
+                else
+                {
+                    MessageBox.Show("El  puerto ya existe", "Mensaje", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+        }
     }
-}
+
