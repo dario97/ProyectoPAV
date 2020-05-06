1308a15b813d651ef351eccc7ff3ebdaaaddd1a3
Javier Alejandro Piotti Trespi <75667@sistemas.frc.utn.edu.ar>
2020-05-06 12:57:10 -0300

    add frm_modificar_puertos

--- ProyectoPAV/gui/frm_modificar_puertos.cs
+++ ProyectoPAV/gui/frm_modificar_puertos.cs
@@ -16,5 +16,10 @@ namespace ProyectoPAV.gui
         {
             InitializeComponent();
         }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
