5ca85576bc1389d5b859abe142a57d4d97de1da0
Admin <Admin@DESKTOP-3MQ0FV8>
2020-05-18 18:06:25 -0300

    asa

---
+++ ProyectoPAV/gui/Frm_Agregar_Itinerario.Designer.cs
@@ -0,0 +1,182 @@
+﻿namespace ProyectoPAV.gui
+{
+    partial class Frm_Agregar_Itinerario
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txt_descripcion = new System.Windows.Forms.TextBox();
+            this.txt_categoria = new System.Windows.Forms.TextBox();
+            this.cmd_agregar = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.dgv1 = new System.Windows.Forms.DataGridView();
+            this.num_escala = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.cod_puerto = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.cmd_aceptar = new System.Windows.Forms.Button();
+            this.cmd_cancelar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txt_descripcion
+            // 
+            this.txt_descripcion.Location = new System.Drawing.Point(125, 61);
+            this.txt_descripcion.Name = "txt_descripcion";
+            this.txt_descripcion.Size = new System.Drawing.Size(100, 20);
+            this.txt_descripcion.TabIndex = 0;
+            // 
+            // txt_categoria
+            // 
+            this.txt_categoria.Location = new System.Drawing.Point(125, 88);
+            this.txt_categoria.Name = "txt_categoria";
+            this.txt_categoria.Size = new System.Drawing.Size(100, 20);
+            this.txt_categoria.TabIndex = 1;
+            // 
+            // cmd_agregar
+            // 
+            this.cmd_agregar.Location = new System.Drawing.Point(125, 125);
+            this.cmd_agregar.Name = "cmd_agregar";
+            this.cmd_agregar.Size = new System.Drawing.Size(75, 23);
+            this.cmd_agregar.TabIndex = 3;
+            this.cmd_agregar.Text = "Agregar";
+            this.cmd_agregar.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(61, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(125, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "AGREGAR ITINERARIO";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(34, 68);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(63, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Descripción";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(34, 95);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(54, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Categoría";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(34, 125);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(83, 13);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Agregar escalas";
+            // 
+            // dgv1
+            // 
+            this.dgv1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgv1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.num_escala,
+            this.cod_puerto});
+            this.dgv1.Location = new System.Drawing.Point(37, 164);
+            this.dgv1.Name = "dgv1";
+            this.dgv1.Size = new System.Drawing.Size(240, 150);
+            this.dgv1.TabIndex = 9;
+            // 
+            // num_escala
+            // 
+            this.num_escala.HeaderText = "Numero de escala";
+            this.num_escala.Name = "num_escala";
+            // 
+            // cod_puerto
+            // 
+            this.cod_puerto.HeaderText = "Código de puerto";
+            this.cod_puerto.Name = "cod_puerto";
+            // 
+            // cmd_aceptar
+            // 
+            this.cmd_aceptar.Location = new System.Drawing.Point(37, 341);
+            this.cmd_aceptar.Name = "cmd_aceptar";
+            this.cmd_aceptar.Size = new System.Drawing.Size(75, 23);
+            this.cmd_aceptar.TabIndex = 10;
+            this.cmd_aceptar.Text = "Aceptar";
+            this.cmd_aceptar.UseVisualStyleBackColor = true;
+            // 
+            // cmd_cancelar
+            // 
+            this.cmd_cancelar.Location = new System.Drawing.Point(202, 341);
+            this.cmd_cancelar.Name = "cmd_cancelar";
+            this.cmd_cancelar.Size = new System.Drawing.Size(75, 23);
+            this.cmd_cancelar.TabIndex = 11;
+            this.cmd_cancelar.Text = "Cancelar";
+            this.cmd_cancelar.UseVisualStyleBackColor = true;
+            // 
+            // Frm_Agregar_Itinerario
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(320, 395);
+            this.Controls.Add(this.cmd_cancelar);
+            this.Controls.Add(this.cmd_aceptar);
+            this.Controls.Add(this.dgv1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cmd_agregar);
+            this.Controls.Add(this.txt_categoria);
+            this.Controls.Add(this.txt_descripcion);
+            this.Name = "Frm_Agregar_Itinerario";
+            this.Text = "Frm_Agregar_Itinerario";
+            ((System.ComponentModel.ISupportInitialize)(this.dgv1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txt_descripcion;
+        private System.Windows.Forms.TextBox txt_categoria;
+        private System.Windows.Forms.Button cmd_agregar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.DataGridView dgv1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn num_escala;
+        private System.Windows.Forms.DataGridViewTextBoxColumn cod_puerto;
+        private System.Windows.Forms.Button cmd_aceptar;
+        private System.Windows.Forms.Button cmd_cancelar;
+    }
+}
